62a4b81318025d94aabe17ae7237097f157e1817
Shantanu Raj <s@sraj.me>
2019-07-27 19:58:23 +0530

    Test alternate action type definition method

--- src/stores/router.ts
+++ src/stores/router.ts
@@ -16,15 +16,11 @@ import { effect, IEffectAction } from './utils';
 /**
  * Action creator for navigating between routes
  */
-interface INavigateAction {
-  type: 'NAVIGATE';
-  route: string;
-}
-const NAVIGATE: INavigateAction['type'] = 'NAVIGATE';
-export const navigate = (route: string): INavigateAction => ({
-  type: NAVIGATE,
+export const navigate = (route: string) => ({
+  type: 'NAVIGATE' as const,
   route,
 });
+type INavigateAction = ReturnType<typeof navigate>;
 
 export type RouterActions = INavigateAction | IEffectAction;
 
@@ -32,7 +28,7 @@ export type RouterActions = INavigateAction | IEffectAction;
  * Router navigation epic
  */
 export const routerEpic: Epic<RouterActions, IEffectAction, IState> = action$ =>
-  action$.ofType(NAVIGATE).pipe(
+  action$.ofType('NAVIGATE').pipe(
     map((action: INavigateAction) => action.route),
     tap(routeTo),
     map(payload => effect({ error: false, epic: 'routerEpic', payload })),
@@ -43,6 +39,6 @@ export const routerEpic: Epic<RouterActions, IEffectAction, IState> = action$ =>
  */
 export const routeTitleSyncEpic: Epic<Actions, ISetTitleAction, IState> = action$ =>
   action$.pipe(
-    filter(action => action.type === NAVIGATE),
+    filter(action => action.type === 'NAVIGATE'),
     map(({ route }: INavigateAction) => setTitle(getTitle(route))),
   );
